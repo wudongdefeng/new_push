0a061a83ade155da80c78a311be5efae2fc06d4a
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-11-11 07:27:05 +0000

    Update cctv.py

--- cctv.py
+++ cctv.py
@@ -1,9 +1,9 @@
-import re 
+import re,os 
 
 import requests 
 
 from lxml import etree 
-
+from os import environ
 import time 
 
 import yagmail 
@@ -89,3 +89,31 @@ def main(event, context):
     yag.send(to=['xxxxxxxx@qq.com'], subject=f'{str_time}新闻联播推送', contents=content) 
 
     return '邮件发送成功' 
+def weixin_push(content):
+    wx_push_token = requests.post(url='https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s'%(wxid,wxsecret),data="").json()['access_token']
+    wx_push_data = {
+            "agentid":1000002,
+            "msgtype":"text",
+            "touser":"@all",
+            "text":{
+                    "content":news
+            },
+            "safe":0
+        }
+    requests.post('https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s'%wx_push_token,json=wx_push_data)
+
+if __name__ == '__main__':
+    #设定天气预报城市与查询时间
+    city_code =os.environ['city_code'] #先在weather.com.cn上查询城市天气，网址结尾的数字替换即可
+    cookie =os.environ['cookie'] #在查询天气的时候，按F12，在控制台复制对应的cookie并填入
+    info_time = datetime.now()
+    timestamps = round(datetime.timestamp(info_time)*1000)
+    #设定企业微信推送参数
+    wxid =os.environ['wxid']
+    wxsecret =os.environ['wxsecret']
+    # 设定新闻时间（当天）与类型
+    #财经：finance_0_suda 社会：news_society_suda 国内：news_china_suda 国际：news_world_suda
+    #科技：tech_news_suda 军事：news_mil_suda 娱乐：ent_suda 体育：sports_suda 总排行：www_www_all_suda_suda
+    news_type = 'news_china_suda'
+    news_time = datetime.strftime(info_time,"%Y%m%d") 
+    weixin_push(message_content(city_code,timestamps,info_time,get_news(news_type,news_time),get_sentence()))

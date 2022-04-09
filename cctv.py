03e1a753ae588851f5a3cde5cb345e5881e30007
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-04-09 16:46:53 +0800

    Update cctv.py

--- cctv.py
+++ cctv.py
@@ -11,7 +11,7 @@ import yagmail
 
 headers = { 
 
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4651.0 Safari/537.36",
+"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4651.0 Safari/537.36",
 
 } 
 

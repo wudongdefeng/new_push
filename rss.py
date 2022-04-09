49b3df0b11043792970d5fb09e4aa153c1766463
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-04-09 16:21:24 +0800

    Update rss.py

--- rss.py
+++ rss.py
@@ -20,7 +20,7 @@ def get_news():
             'content': f'{content}\nfrom rss news'
         })
     return result
- def weixin_push(result):
+def weixin_push(result):
     wx_push_token = requests.post(url='https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s'%(wxid,wxsecret),data="").json()['access_token']
     wx_push_data = {
             "agentid":1000008,
@@ -33,7 +33,7 @@ def get_news():
         }
     requests.post('https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s'%wx_push_token,json=wx_push_data)
     
-    if __name__ == '__main__':      
+   if __name__ == '__main__':      
     wxid = os.environ['wxid']
     wxsecret = os.environ['wxsecret']    
     weixin_push(get_news())

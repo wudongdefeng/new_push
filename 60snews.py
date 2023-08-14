7f3cab17b54af2bce1c3c3a6460f836e0d1c0300
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2023-08-14 19:51:54 +0800

    Update 60snews.py

--- 60snews.py
+++ 60snews.py
@@ -1,21 +1,21 @@
 import os
 import requests
-
+import json
 
 text_url = "https://60s.viki.moe/60s?e=text"
 
 text_response = requests.get(text_url)
 content = text_response.text
 QYWX_KEY = get_environ("webhook")
-    webhook = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" + QYWX_KEY
-    headers = {"Content-Type": "text/plain"}
-    data = {
-        "msgtype": "text",
-        "text": {
-            "content": content
-        }
-
+webhook = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" + QYWX_KEY
+headers = {"Content-Type": "text/plain"}
+data = {
+    "msgtype": "text",
+    "text": {
+        "content": content
     }
+
+}
 
 if QYWX_KEY != "":
     r = requests.post(url=webhook, headers=headers, json=data).json()

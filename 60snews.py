d9118ce1e59da7c93436aca5314a5aa744ac5167
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2023-08-14 19:48:09 +0800

    Create 60snews.py

---
+++ 60snews.py
@@ -0,0 +1,28 @@
+import os
+import requests
+
+
+text_url = "https://60s.viki.moe/60s?e=text"
+
+text_response = requests.get(text_url)
+content = text_response.text
+QYWX_KEY = get_environ("webhook")
+    webhook = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" + QYWX_KEY
+    headers = {"Content-Type": "text/plain"}
+    data = {
+        "msgtype": "text",
+        "text": {
+            "content": content
+        }
+
+    }
+
+if QYWX_KEY != "":
+    r = requests.post(url=webhook, headers=headers, json=data).json()
+    if r["errmsg"] == "ok":
+        print("企业微信机器人推送成功")
+    else:
+        print("企业微信机器人推送失败")
+    print()
+else:
+    print()
